ea326302e50442fe1b535093fb003baa20cd8b5b
Ariel <arielrodriguez0@outlook.com>
2021-12-30 00:48:12 -0600

    First_Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import {NavLink, Switch, Route} from 'react-router-dom';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Contenedor>
+       <Menu>
+          <NavLink to="/">Inicio</NavLink>
+          <NavLink to="/Blog">Blog</NavLink>
+          <NavLink to="/Tienda">Tienda</NavLink>
+       </Menu>
+
+       <main>
+
+         <Switch>
+            <Route path="/" element={} ></Route>
+         </Switch>
+
+       </main>
+    </Contenedor>
+
   );
 }
-
+ 
 export default App;
+
+
+
+const Contenedor = styled.div`
+    max-width: 1000px;
+    padding: 40px;
+    width: 90%;
+    display: grid;
+    gap: 20px;
+    grid-template-columns: 2fr 1fr;
+    background: #fff;
+    margin: 40px 0;
+    border-radius: 10px;
+    box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+`;
+ 
+const Menu = styled.nav`
+    width: 100%;
+    text-align: center;
+    background: #092c4c;
+    grid-column: span 2;
+    border-radius: 3px;
+ 
+    a {
+        color: #fff;
+        display: inline-block;
+        padding: 15px 20px;
+    }
+ 
+    a:hover {
+        background: #1d85e8;
+        text-decoration: none;
+    }
+`;
